b7e1bc9d30012382b9e01efb9c6ea96309232732
krauzX <aryanrajapandey@gmail.com>
2023-07-01 23:47:43 +0530

    auth

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="AuthPage" class="w-full h-[100vh] bg-white">
         <div class="w-full flex items-center justify-center p-5 border-b border-b-gray-300">
-            <NuxtLink to="/" class="min-w-[170px]">
+            <NuxtLink to="/Aryusa" class="min-w-[170px]">
                 <img width="170" src="/AliExpress-logo.png">
             </NuxtLink>
         </div>
